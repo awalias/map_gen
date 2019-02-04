2e5d20574a86c6f612eb72859f55b8659c3e2821
awalias <antwilson@hotmail.co.uk>
2019-02-04 16:01:48 +0800

    added yellow motorways brum brum

--- src/MapGen.ts
+++ src/MapGen.ts
@@ -2,10 +2,12 @@ import {rand, point, angle, Coordinate} from './utils';
 
 export class MapGen {
   private debug_mode: boolean = false;
-  private COLOR_SEA_BLUE: string = "#7981c9";
-  private COLOR_LAND_GREEN: string = "#bcae86";
-  private COLOR_TEXT_SHADOW: string = "#ffffff"
-  private COLOR_TEXT_LABEL: string = "#000000";
+  private color_sea_blue: string = "#7981c9";
+  private color_land_green: string = "#bcae86";
+  private color_text_shadow: string = "#ffffff"
+  private color_text_label: string = "#000000";
+  private color_motorway_main: string = "#ebc247"
+  private motorway_line_width: number = 2;
   private context: CanvasRenderingContext2D;
 
   private number_of_guide_points: number;
@@ -15,11 +17,9 @@ export class MapGen {
   private minor_city_min: number = 1;
   private minor_city_max: number = 5;
   private motorways_min: number = 1;
-  private motorway_max: number = 5;
+  private motorways_max: number = 5;
   private major_roads_min: number = 5;
   private major_roads_max: number = 10;
-  private minor_roads_min: number = 10;
-  private minor_roads_max: number = 30;
   private county_border_points_min: number = 0;
   private county_border_points_max: number = 5;
   private county_border_count_min: number = 0;
@@ -39,6 +39,8 @@ export class MapGen {
   private border_points: Coordinate[] = [];
   private major_cities: Coordinate[] = [];
   private minor_cities: Coordinate[] = [];
+
+  private motorways: Coordinate[][] = [];
 
   private major_city_labels: string[] = [];
   private minor_city_labels: string[] = [];
@@ -128,7 +130,7 @@ export class MapGen {
 
       for (let j=1; j<max_inner_iterations; j++) {
 
-        let new_point: Coordinate = this.getRandomBorderPoint(current_point, this.guide_points[i], j, max_inner_iterations);
+        let new_point: Coordinate = this.getRandomIntermediatePoint(current_point, this.guide_points[i], j, max_inner_iterations);
         this.border_points.push(new_point);
         current_point = new_point;
 
@@ -152,14 +154,14 @@ export class MapGen {
     return point;
   }
 
-  getRandomBorderPoint(current_point: Coordinate, target_point: Coordinate, j: number, n_steps: number) {
+  getRandomIntermediatePoint(current_point: Coordinate, target_point: Coordinate, j: number, n_steps: number, min_noise_seed: number = -0.2, max_noise_seed: number = 0.2) {
     const abs_distance = Math.hypot(target_point[0]-current_point[0], target_point[1]-current_point[1]);
-    const min_noise: number = -0.2 * abs_distance;
-    const max_noise: number = 0.2 * abs_distance;
+    const min_noise: number = min_noise_seed * abs_distance;
+    const max_noise: number = max_noise_seed * abs_distance;
     let step_distance = j/n_steps;
     let intermediate_point: Coordinate = [0,0];
 
-    // get point on line 1/10th the way from current to target
+    // get point on line 1/n_steps the way from current to target
     let slope = (target_point[1]-current_point[1])/(target_point[0]-current_point[0])
     let intermediate_point_x = (target_point[0]-current_point[0]) * step_distance + current_point[0]
     let intermediate_point_y = slope * (intermediate_point_x-current_point[0]) + current_point[1]
@@ -169,6 +171,70 @@ export class MapGen {
     return intermediate_point;
   }
 
+  generateMotorways() {
+    for (let i=0; i<rand(this.motorways_min, this.motorways_max); i++) {
+      this.motorways.push(this.generateMotorway());
+    }
+  }
+
+  generateMotorway(): Coordinate[] {
+    let motorway: Coordinate[] = [];
+    let starting_point: Coordinate = [0, 0];
+    let current_point: Coordinate;
+    let destination_point: Coordinate = [0, 0];
+    let n_steps: number = rand(30, 40);
+
+    // motorway will connect major city (or random point) and a random point
+    let starting_city: Coordinate =  rand(0, 2) ? this.getRandomPointOnLand() : this.major_cities[rand(0, this.major_cities.length-1)];
+    let destination_city: Coordinate = this.getRandomPointOnLand();
+
+
+    // motorway doesn't have to start in the city centre - add some noise
+    let safety_count: number = 0;
+    while (!this.context.isPointInPath(starting_point[0], starting_point[1])) {
+      starting_point = [starting_city[0] + rand(-20,20), starting_city[1] + rand(-20, 20)];
+
+      safety_count++;
+      if (safety_count > 10) {
+        return [];
+      }
+    }
+
+    motorway.push(starting_point);
+    current_point = starting_point;
+
+    // motorway doesn't have to end in the city centre - add some noise
+    safety_count = 0;
+    while (!this.context.isPointInPath(destination_point[0], destination_point[1])) {
+      destination_point = [destination_city[0] + rand(-20,20), destination_city[1] + rand(-20, 20)];
+
+      safety_count++;
+      if (safety_count > 10) {
+        return [];
+      }
+    }
+
+    for (let i=0; i<n_steps; i++) {
+      let new_point: Coordinate = [0, 0];
+      safety_count = 0;
+      while (!this.context.isPointInPath(new_point[0], new_point[1])) {
+        new_point = this.getRandomIntermediatePoint(current_point, destination_point, i, n_steps, -0.1, 0.1);
+
+        safety_count++;
+        if (safety_count > 10) {
+          return [];
+        }
+      }
+      motorway.push(new_point);
+      current_point = new_point;
+    }
+
+    // pop the first point since it occasionally comes out looking strange
+    motorway.shift();
+    motorway.shift();
+    return motorway;
+  }
+
   plot() {
     this.generateRandomGuidePoints();
     this.generateBorder();
@@ -176,6 +242,7 @@ export class MapGen {
 
   plotIsPointInPathMethods() {
     this.generateRandomCityPoints();
+    this.generateMotorways();
   }
 
   drawIsPointInPathMethods() {
@@ -184,10 +251,12 @@ export class MapGen {
       point(this.major_cities[i][0], this.major_cities[i][1], this.major_city_point_radius, this.context);
       this.context.font = "14px Helvetica";
 
-      this.context.shadowColor = this.COLOR_TEXT_SHADOW;
+      this.context.shadowColor = this.color_text_shadow;
       this.context.shadowBlur = 4;
-      this.context.fillStyle = this.COLOR_TEXT_LABEL;
+      this.context.fillStyle = this.color_text_label;
       this.context.fillText(this.major_city_labels[i], this.major_cities[i][0]+this.text_offset[0], this.major_cities[i][1]+this.text_offset[1]);
+      this.context.shadowColor = "rgba(0,0,0,0)";
+      this.context.shadowBlur = 0;
     }
 
     // draw minor cities
@@ -195,10 +264,26 @@ export class MapGen {
       point(this.minor_cities[i][0], this.minor_cities[i][1], this.minor_city_point_radius, this.context);
       this.context.font = "12px Helvetica";
 
-      this.context.shadowColor = this.COLOR_TEXT_SHADOW;
+      this.context.shadowColor = this.color_text_shadow;
       this.context.shadowBlur = 4;
-      this.context.fillStyle = this.COLOR_TEXT_LABEL;
+      this.context.fillStyle = this.color_text_label;
       this.context.fillText(this.minor_city_labels[i], this.minor_cities[i][0]+this.text_offset[0], this.minor_cities[i][1]+this.text_offset[1]);
+      this.context.shadowColor = "rgba(0,0,0,0)";
+      this.context.shadowBlur = 0;
+    }
+
+    // draw motorways
+    for (let i=0; i<this.motorways.length; i++) {
+      this.context.strokeStyle = this.color_motorway_main;
+      this.context.lineWidth = this.motorway_line_width;
+      this.context.beginPath();
+      this.context.moveTo(this.motorways[i][0][0], this.motorways[i][0][1]);
+
+      // connect cirlce plot points
+      for (let j=1; j<this.motorways[i].length; j++) {
+        this.context.lineTo(this.motorways[i][j][0], this.motorways[i][j][1]);
+        this.context.stroke();
+      }
     }
   }
 
@@ -208,7 +293,7 @@ export class MapGen {
     }
 
     // fill background
-    this.context.fillStyle = this.COLOR_SEA_BLUE;
+    this.context.fillStyle = this.color_sea_blue;
     this.context.fillRect(0, 0, 600, 600);
 
     // make regular drawings
@@ -229,7 +314,7 @@ export class MapGen {
     this.plotIsPointInPathMethods();
 
     // fill land
-    this.context.fillStyle = this.COLOR_LAND_GREEN;
+    this.context.fillStyle = this.color_land_green;
     this.context.fill();
 
     // draw other features that rely on point in path method
